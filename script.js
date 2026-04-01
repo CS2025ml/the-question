e1de83cde1cd04ed6a3406d7b9f381c5ec1b3894
CS2025ml <torresag@iu.edu>
2026-04-01 19:47:45 -0400

    Adjust noBtn transition duration and movement range

--- script.js
+++ script.js
@@ -41,11 +41,11 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   /* NO BUTTON RUNS */
- noBtn.style.transition = "transform 0.6s ease";
+ noBtn.style.transition = "transform 0.2s ease";
 
 noBtn.addEventListener("mouseover", () => {
-  const x = Math.random() * 200 - 100;
-  const y = Math.random() * 200 - 100;
+  const x = Math.random() * 500 - 300;
+  const y = Math.random() * 500 - 300;
   noBtn.style.transform = `translate(${x}px, ${y}px)`;
 });
 
